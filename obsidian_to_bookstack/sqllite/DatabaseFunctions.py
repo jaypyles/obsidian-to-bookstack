7247d9cae9b37dde6735dc49169e89d892d58449
useahawk <u.seahawk@gmx.net>
2024-01-23 16:16:36 +0100

    Update DatabaseFunctions.py typo
    quotation marks caused errror in my environment
    pls test & confirm

--- obsidian_to_bookstack/sqllite/DatabaseFunctions.py
+++ obsidian_to_bookstack/sqllite/DatabaseFunctions.py
@@ -1,7 +1,7 @@
 import os
 import sqlite3
 
-DATA_PATH = f"/home/{os.environ.get("USER")}/.config/obsidian_to_bookstack/data"
+DATA_PATH = f"/home/{os.environ.get('USER')}/.config/obsidian_to_bookstack/data"
 
 def connect():
     conn = sqlite3.connect(f"{DATA_PATH}/settings.db")
